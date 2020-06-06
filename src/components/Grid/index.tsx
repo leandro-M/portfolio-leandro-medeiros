776fdc8b24a6df94b3421cac8379056c831100dc
Leandro Medeiros <leandro.medeiros@pixter.com.br>
2020-06-06 14:09:25 -0300

    feat(header): create a grid component

---
+++ src/components/Grid/index.tsx
@@ -0,0 +1,13 @@
+﻿import React from "react";
+
+import { Container } from "./styles";
+
+interface Props {
+  children?: React.Component;
+}
+
+const Grid: React.FC<Props> = ({ children }) => {
+  return <Container>{children}</Container>;
+};
+
+export default Grid;
---
+++ src/components/Grid/styles.ts
@@ -0,0 +1,6 @@
+﻿import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
